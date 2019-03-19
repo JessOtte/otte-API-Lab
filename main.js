afb9526f70fea080bdacc75549819a86172803cf
Jessica Otte <40188802+semiottematic@users.noreply.github.com>
2019-03-19 13:18:19 -0400

    Added a box around each div
    Need to figure out how to add a border/box around each post, not each div.

--- main.js
+++ main.js
@@ -10,12 +10,24 @@ $(() => {
        title =(data.data.children[i].data.title);
        img =(data.data.children[i].data.thumbnail);
        permalink = (data.data.children[i].data.permalink);
+       box = (data.data.children[i]);
+
+
+       let posts = $( '#post1, #post2, #post3, #post4, #post5, #post6, #post7, #post8, #post9, #post10');
+
 
        console.log(title, img, permalink);   
 
-       $('.posts-container').append(`<div><a href="https://www.reddit.com/${permalink}"><img src="${img}"></img><br><div id="title-container">${title}</div><br><div id="link-container">Link: ${permalink}</div><br></a></div>`);
-        return i < 10;
+       $('.posts-container').append(`<h4 id="title-container">${title}</h4><div><a href="https://www.reddit.com/${permalink}"><img src="${img}"></img><br><div id="link-container">Link: ${permalink}</div><br></a></div>`);
+       return i < 10;
+    
+
+
     });
+
+
+
+
 
 
     
